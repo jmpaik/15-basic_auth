932a78d86918eada6c65b67553a87b4b15e02a8c
JINHO PAIK <JMPAIK83@GMAIL.COM>
2017-01-05 22:35:56 -0800

    update travis aws mocks and test env file

--- lab-jinho/test/lib/aws-mocks.js
+++ lab-jinho/test/lib/aws-mocks.js
@@ -2,22 +2,32 @@
 
 const AWS = require('aws-sdk-mock');
 
-module.exports = exports {};
+module.exports = exports = {};
 
 exports.uploadMock = {
-  ETag: '1234abcd',
-  Location: 'http://mockurl.com/mock.png',
+  ETag: '"1234abcd"',
+  Location: 'https://mockurl.com/mock.png',
   Key: '1234.png',
-  Key: '1234.png',
+  key: '1234.png',
   Bucket: 'lab17'
 };
 
-AWS.mock('S3', 'upload', function(paras, callback) {
+AWS.mock('S3', 'upload', function(params, callback) {
   if (!params.ACL === 'public-read') {
     return callback(new Error('ACL must be public-read'));
   };
 
   if (!params.Bucket === 'lab17') {
-    return callback(new Error('Bucket must be lab17'));
+    return callback(new Error('Bucket must be cfgram'));
   };
-})
+
+  if (!params.Key) {
+    return callback(new Error('Key required'));
+  };
+
+  if (!params.Body) {
+    return callback(new Error('Body required'));
+  };
+
+  callback(null, exports.uploadMock);
+});
